bf32d4371478209391c1eb3973b4c5fceccfea44
Mohammed Subhani <mohammedsubhani201@gmail.com>
2025-11-06 23:27:32 +0530

    adding 2 number without using temp testing

--- learning.c
+++ learning.c
@@ -2,7 +2,10 @@
 
 int main() {
     int a,b;
-    scanf ("%d,%d",&a,&b);
-    printf("%d\n",a-b);
+    printf("Enter the value for a ");
+    scanf("%d",&a);
+    printf("Enter the value for b");
+    scanf("%d",&b);
+    printf("the addition of two number is %d",a+b);
     return 0;
 }
